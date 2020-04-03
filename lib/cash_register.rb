479fea1b65350c2c2a7408c9ad9ed2db4a8bd4a2
Christopher Reinken <christopherreinken@gmail.com>
2020-04-03 16:50:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   end
   
   def apply_discount
-    @total += @discount
+    @total -= (@total * .@discount)
     message = "After the discount, the total comes to $#{@total}"
   end
   
